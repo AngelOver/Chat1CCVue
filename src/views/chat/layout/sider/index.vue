34e5b880c48f92876864b37b4e9e94f9b9eb6b38
zhangliang <2424123787@qq.com>
2023-05-15 22:13:58 +0800

    首页优化

--- src/views/chat/layout/sider/index.vue
+++ src/views/chat/layout/sider/index.vue
@@ -86,16 +86,16 @@ watch(
         </div>
 				<div class="p-4 " style="padding-top: 0px;padding-bottom: 10px;">
 
-					<h6 style="font-size: 14.5px">
-						<strong><a href="https://1chat.cc" target="_blank" >1Chat.cc 免费、无限制、免登最快 </a> </strong>
-					</h6>
+<!--					<h6 style="font-size: 14.5px">-->
+<!--						<strong><a href="https://1chat.cc" target="_blank" >1Chat.cc 免费、无限制、免登最快 </a> </strong>-->
+<!--					</h6>-->
 				<!-- 	<h6 style="margin-bottom: 0px;color: #c18401;font-size: x-small">
 						永久域名：<a href="https://1chat.cc" target="_blank"> https://1chat.cc  </a>
 					</h6> -->
 
-					<h6 style="margin-bottom:4rem;;font-size: 12px;opacity: 0.5;text-align: right">
-						“最快”：100字返回5秒
-					</h6>
+<!--					<h6 style="margin-bottom:5rem;;font-size: 12px;opacity: 0.5;text-align: right">-->
+<!--						“最快”：100字返回5秒-->
+<!--					</h6>-->
 <!--					<h6 style="margin-bottom:5rem;color: #c18401;font-size:  xx-small">-->
 <!--						是快是慢，看你们运气 ╮(￣▽￣)╭ 成本4:1-->
 <!--					</h6>-->
@@ -111,15 +111,15 @@ watch(
 <!--						</h6>-->
 <!--					</h6>-->
 
-					<h6 style="color: #c18401;font-size: 12.5px">
-						<a :href="imageUrl_wxzs" target="_blank"><strong>服务器昂贵，接口昂贵，但网站免费！！</strong> </a>
-					</h6>
-					<h6 style="color: rgb(50 197 157);font-size: 12px">
-						<a :href="imageUrl_wxzs" target="_blank">每人每月捐个三元、服务就能永久免费下去 </a>
-					</h6>
-					<h6 style="opacity: 0.7;text-align: right">
-						<a style="padding-left: 50%;" :href="imageUrl_wxzs" target="_blank">点击->赞助 </a>
-					</h6>
+<!--					<h6 style="color: #c18401;font-size: 12.5px">-->
+<!--						<a :href="imageUrl_wxzs" target="_blank"><strong>服务器昂贵，接口昂贵，但网站免费！！</strong> </a>-->
+<!--					</h6>-->
+<!--					<h6 style="color: rgb(50 197 157);font-size: 12px">-->
+<!--						<a :href="imageUrl_wxzs" target="_blank">每人每月捐个三元、服务就能永久免费下去 </a>-->
+<!--					</h6>-->
+<!--					<h6 style="opacity: 0.7;margin-bottom:3rem;text-align: right">-->
+<!--						<a style="padding-left: 50%;" :href="imageUrl_wxzs" target="_blank">点击->赞助 </a>-->
+<!--					</h6>-->
 
 <!--					<h6 style="color: rgb(50 197 157);font-size: xx-small">-->
 <!--						注:如果你懂技术,有能力,有apiKey,可前往-->
@@ -141,7 +141,9 @@ watch(
 				<div class="p-4 " style="padding-top: 0;">
 					<dev class="flex items-center justify-between  " style="padding-bottom: 0.3rem" >
 						<div class="flex-1 flex-shrink-0 overflow-hidden">
-
+							<p style="" >
+								很多网站不在了
+							</p>
 							<p style="color: #c18401" >
 								加群：--> 防走丢
 							</p>
@@ -158,21 +160,21 @@ watch(
 						</div>
 					</dev>
 
-					<h6 style="">
-					<a  style="color:rgb(99 165 237);" href="https://fk.a3e.top?code=YT0xJmI9NQ%3D%3D" target="_blank">	批发：批发ChatGPT特价号 3.3元 </a>
-					</h6>
-	<!-- 				<h6 style="font-size: x-small">
-						<a style="color: #c18401" href="https://fk.a3e.top?code=YT00JmI9Mw%3D%3D" target="_blank">	研报：GPT专题行业研报与投资趋势8.8元  </a>
-					</h6> -->
+<!--				<h6 style="">-->
+<!--					<a  style="color:rgb(99 165 237);" href="https://fk.a3e.top?code=YT0xJmI9NQ%3D%3D" target="_blank"> 批发请点击-> 5刀、120刀账号下单 </a>-->
+<!--					</h6>-->
+<!--				<h6 style="font-size: x-small">-->
+<!--							<a style="color: #c18401" href="https://fk.a3e.top?code=YT00JmI9Mw%3D%3D" target="_blank">	研报：GPT专题行业研报与投资趋势6.8元  </a>-->
+<!--					</h6>-->
 
-					<h6 style="font-size: 12px">
-						<a style=";opacity: 0.7;" href="https://fk.a3e.top?code=YT0xJmI9Nw%3D%3D" target="_blank">	外链：ChatGPT余额120美元账号 29元  </a>
-					</h6>
+<!--					<h6 style="font-size: 12px">-->
+<!--						<a style=";opacity: 0.7;" href="https://fk.a3e.top?code=YT0xJmI9Nw%3D%3D" target="_blank">	外链：ChatGPT余额120美元账号 29元  </a>-->
+<!--					</h6>-->
 					<!-- 	<h6 style="font-size: 12px">
 						<a style="color: #c18401;opacity: 0.5;" href="https://fk.a3e.top?code=YT0xJmI9Nw%3D%3D" target="_blank">	外链：ChatGPT余额120美元账号 29元  </a>
 					</h6> -->
 					<h6 style="color: rgb(50 197 157);">
-						<a href="https://fk.a3e.top?code=YT0xJmI9Mg%3D%3D" target="_blank">外链：ChatGPT成品账号 9.9元 </a>
+						<a href="https://fk.a3e.top?code=YT0xJmI9Mg%3D%3D" target="_blank">外链：ChatGPT成品独享账号 9.9元  </a>
 					</h6>
 
 					<NButton block @click="show = true">
